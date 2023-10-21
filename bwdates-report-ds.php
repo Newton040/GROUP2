f8483facf7fffdb7f13a2bf68f5e176d579b8d68
Newton <148640629+Newton040@users.noreply.github.com>
2023-10-22 03:57:03 +0800

    Add files via upload

--- bwdates-report-ds.php
+++ bwdates-report-ds.php
@@ -3,26 +3,6 @@ include_once('../includes/config.php');
 if (strlen($_SESSION['adminid']==0)) {
   header('location:logout.php');
   } else{
- // for  password change   
-if(isset($_POST['update']))
-{
-$oldpassword=md5($_POST['currentpassword']); 
-$newpassword=md5($_POST['newpassword']);
-$sql=mysqli_query($con,"SELECT password FROM admin where password='$oldpassword'");
-$num=mysqli_fetch_array($sql);
-if($num>0)
-{
-$adminid=$_SESSION['adminid'];
-$ret=mysqli_query($con,"update admin set password='$newpassword' where id='$adminid'");
-echo "<script>alert('Password Changed Successfully !!');</script>";
-echo "<script type='text/javascript'> document.location = 'change-password.php'; </script>";
-}
-else
-{
-echo "<script>alert('Old Password not match !!');</script>";
-echo "<script type='text/javascript'> document.location = 'change-password.php'; </script>";
-}
-}
 
     
 ?>
@@ -34,22 +14,11 @@ echo "<script type='text/javascript'> document.location = 'change-password.php';
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
         <meta name="description" content="" />
         <meta name="author" content="" />
-        <title>Change password | LAWS CAMPUS SCHOOL FEES MANAGEMENT SYSTEM</title>
+        <title>Between Dates report date selection |LAWS CAMPUS SCHOOL FEES MANAGEMENT SYSTEM</title>
         <link href="https://cdn.jsdelivr.net/npm/simple-datatables@latest/dist/style.css" rel="stylesheet" />
         <link href="../css/styles.css" rel="stylesheet" />
         <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/js/all.min.js" crossorigin="anonymous"></script>
-<script language="javascript" type="text/javascript">
-function valid()
-{
-if(document.changepassword.newpassword.value!= document.changepassword.confirmpassword.value)
-{
-alert("Password and Confirm Password Field do not match  !!");
-document.changepassword.confirmpassword.focus();
-return false;
-}
-return true;
-}
-</script>
+
 
     </head>
     <body class="sb-nav-fixed">
@@ -61,26 +30,23 @@ return true;
                     <div class="container-fluid px-4">
                         
 
-                        <h1 class="mt-4">Change Password</h1>
+                        <h1 class="mt-4">B/w Dates Report Date Selection</h1>
                         <div class="card mb-4">
-                     <form method="post" name="changepassword" onSubmit="return valid();">
+                     <form method="post" name="bwdatesreport" action="bwdates-report-result.php" >
                             <div class="card-body">
                                 <table class="table table-bordered">
                                    <tr>
-                                    <th>Current Password</th>
-                                       <td><input class="form-control" id="currentpassword" name="currentpassword" type="password" value="" required /></td>
+                                    <th>From Date</th>
+                                       <td><input class="form-control" id="fromdate" name="fromdate" type="date" value="" required /></td>
                                    </tr>
                                    <tr>
-                                       <th>New Password</th>
-                                       <td><input class="form-control" id="newpassword" name="newpassword" type="password" value=""  required /></td>
+                                       <th>To Date</th>
+                                       <td><input class="form-control" id="todate" name="todate" type="date" value=""  required /></td>
                                    </tr>
-                                         <tr>
-                                       <th>Confirm Password</th>
-                                       <td colspan="3"><input class="form-control" id="confirmpassword" name="confirmpassword" type="password"    required /></td>
-                                   </tr>
+                                      
                   
                                    <tr>
-                                       <td colspan="4" style="text-align:center ;"><button type="submit" class="btn btn-primary btn-block" name="update">Change</button></td>
+                                       <td colspan="4" style="text-align:center ;"><button type="submit" class="btn btn-primary btn-block" name="submit">Submit</button></td>
 
                                    </tr>
                                     </tbody>
@@ -92,7 +58,7 @@ return true;
 
                     </div>
                 </main>
-         
+          
             </div>
         </div>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
