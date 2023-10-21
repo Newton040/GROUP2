ce43b2279f865f6a614f8e152a396ee53d1da715
Newton <148640629+Newton040@users.noreply.github.com>
2023-10-22 03:58:45 +0800

    Add files via upload

---
+++ sidebar.php
@@ -0,0 +1,43 @@
+  <div id="layoutSidenav_nav">
+                <nav class="sb-sidenav accordion sb-sidenav-dark" id="sidenavAccordion">
+                    <div class="sb-sidenav-menu">
+                        <div class="nav">
+                            <div class="sb-sidenav-menu-heading"></div>
+                            <a class="nav-link" href="dashboard.php">
+                                <div class="sb-nav-link-icon"><i class="fas fa-tachometer-alt"></i></div>
+                                DASHBOARD
+                            </a>
+
+                            <a class="nav-link" href=".php">
+                                <div class="sb-nav-link-icon"><i class=""></i></div>
+                                Manage Payments
+                            </a>
+                            <a class="nav-link" href=" Notifications.php">
+                                <div class="sb-nav-link-icon"><i class=""></i></div>
+                                Manage  Notifications 
+                            </a>
+                            <a class="nav-link" href="bwdates-report-ds.php">
+                                <div class="sb-nav-link-icon"><i class=""></i></div>
+                                B/W Dates Report
+                            </a>
+                 
+                            <a class="nav-link" href="manage-users.php">
+                                <div class="sb-nav-link-icon"><i class=""></i></div>
+                                Manage Users
+                            </a>
+                            <a class="nav-link" href="add-users.php">
+                                <div class="sb-nav-link-icon"><i class=""></i></div>
+                                Add Users
+                            </a>
+
+ 
+
+                            <a class="nav-link" href="logout.php">
+                                <div class="sb-nav-link-icon"><i class="."></i></div>
+                                Signout
+                            </a>
+                        </div>
+                    </div>
+                
+                </nav>
+            </div>
